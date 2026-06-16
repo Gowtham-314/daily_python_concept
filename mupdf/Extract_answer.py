8282e284e9e0fbdb8b62eaac5a860d467acc5286
Gowtham Gowda C B <gowdagowtham7930@gmail.com>
2026-06-17 03:48:27 +0530

    Generate and save embeddings from PDF

---
+++ mupdf/Extract_answer.py
@@ -0,0 +1,28 @@
+import re
+
+from pathlib import Path
+import fitz
+
+pdf_path = Path(__file__).with_name("questions.pdf")
+
+target_question = "What is Machine Learning?"
+
+doc = fitz.open(pdf_path)
+
+text = ""
+
+for page in doc:
+    text += page.get_text()
+
+doc.close()
+
+pattern = rf"{re.escape(target_question)}\s*Answer:\s*(.*?)(?=Q\d+\.|$)"
+
+match = re.search(pattern, text, re.DOTALL)
+
+if match:
+    answer = match.group(1).strip()
+    print(f"Question: {target_question}\n")
+    print(answer)
+else:
+    print("Question not found.")
--- mupdf/embedded.py
+++ mupdf/embedded.py
@@ -1,27 +1,58 @@
 from pathlib import Path
 import fitz
 
-pdf_path = Path(__file__).with_name("sample.pdf")
-doc = fitz.open(pdf_path)
-
+pdf_path = Path(__file__).with_name("questions.pdf")
 text = ""
-for page in doc:
-    text += page.get_text()
-
-doc.close()
+with fitz.open(pdf_path) as doc:
+    for page in doc:
+        text += page.get_text()
 
 
 # Split the text into chunks of 1000 characters
 
 chunk_size = 1000
-
 chunks = []
-
 for i in range(0, len(text), chunk_size):
     chunks.append(text[i:i+chunk_size])
 
 print(len(chunks))
+    
+    
+# Generate embeddings
+from sentence_transformers import SentenceTransformer
 
-for chunk in chunks:
-    print(chunk)
-    print("-" * 40)
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+embeddings = model.encode(chunks)
+print(embeddings.shape)
+
+
+# Save embeddings
+import numpy as np
+np.save(
+    f"{pdf_path.parent}/embeddings.npy",
+    embeddings
+)
+
+
+# Load embeddings
+embeddings = np.load(
+    f"{pdf_path.parent}/embeddings.npy"
+)
+
+print("\n\n---- Loaded Embeddings ---")
+
+query = "What is a database?"
+query_embedding = model.encode(query)
+
+
+from sklearn.metrics.pairwise import cosine_similarity
+
+scores = cosine_similarity(
+    [query_embedding],
+    embeddings  
+)
+
+best_index = scores.argmax()
+
+print(chunks[best_index])
